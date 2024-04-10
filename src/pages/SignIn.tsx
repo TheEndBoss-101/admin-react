6a9dbd442cac8faa79918396e2f4447b98b5c449
TheEndBoss_101 <94846253+TheEndBoss-101@users.noreply.github.com>
2024-04-10 13:29:33 -0500

    autoComplete

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -6,14 +6,14 @@ function SignIn() {
       <div className="card-body">
         <h2 className="h2 text-center mb-4">Login to your account</h2>
 
-		    <form action="{{ site.base }}/" method="get" autocomplete="off" novalidate>
+		    <form action="{{ site.base }}/" method="get" autoComplete="off" novalidate>
         	<div className="mb-3">
         		<label className="form-label">Username</label>
-        		<input type="text" className="form-control" placeholder="Your username" autocomplete="off" />
+        		<input type="text" className="form-control" placeholder="Your username" autoComplete="off" />
         	</div>
         	<div className="mb-2">
         		<label className="form-label">Password</label>
-        		<input type="text" className="form-control" placeholder="Your password" autocomplete="off" />
+        		<input type="text" className="form-control" placeholder="Your password" autoComplete="off" />
         	</div>
         	<div className="form-footer">
         		<button type="submit" className="btn btn-primary w-100">Sign in</button>
