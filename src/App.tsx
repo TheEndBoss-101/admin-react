0cd0c6ad4fc9116e1689ce54f16532e0cea525be
TheEndBoss_101 <94846253+TheEndBoss-101@users.noreply.github.com>
2024-04-10 13:44:47 -0500

    add home

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,13 @@ import {
 	Route,
 } from "react-router-dom";
 import SignIn from "./pages/SignIn";
+import index from "./pages/index";
 
 function App() {
 	return (
 		<Router>
 			<Routes>
+                <Route exact path="/" element={<index />} />
 				<Route path="/Sign-In" element={<SignIn />} />
 			</Routes>
 		</Router>
--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -6,7 +6,7 @@ function SignIn() {
       <div className="card-body">
         <h2 className="h2 text-center mb-4">Login to your account</h2>
 
-		    <form action="{{ site.base }}/" method="get" autoComplete="off" noValidate>
+		    <form action="/" method="get" autoComplete="off" noValidate>
         	<div className="mb-3">
         		<label className="form-label">Username</label>
         		<input type="text" className="form-control" placeholder="Your username" autoComplete="off" />
